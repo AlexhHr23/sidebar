b6294e7adf4e2a8dec1b91de59d84d1ca7320b28
Alexhr <alex.hernandez24kb@gmail.com>
2023-04-04 00:31:04 -0600

    Creación del sidebar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Sidebar from './components/Sidebar'
+
+function App() {
+  return (
+    <div className="main">
+      <Sidebar />
+      <div className="container">
+        <h1 className="title">UTTLAXCALA</h1>
+        <p className="info">Seguimiento academico</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
